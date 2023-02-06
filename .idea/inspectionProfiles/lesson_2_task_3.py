4311e5a22560c22b271b997c4c843fbbeb7d3adf
SenyukAV <avsenyuk@hotmail.com>
2023-02-06 20:26:10 +0300

    20:25

---
+++ .idea/inspectionProfiles/lesson_2_task_3.py
@@ -0,0 +1,6 @@
+name="Alex"
+age=20
+city="Paris"
+print("My name is %s. I am %d years old. I am from %s " % (name,age,city))
+print("My name is {}. I am {} years old. I am from {} ".format (name,age,city))
+print(f"My name is {name}. I am {age} years old. I am from {city}")
