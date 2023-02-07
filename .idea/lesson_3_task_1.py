56fdb79a02af10d398c3d8c84705f13615e38562
SenyukAV <avsenyuk@hotmail.com>
2023-02-07 23:35:11 +0300

    22:43

--- .idea/lesson_3_task_1.py
+++ .idea/lesson_3_task_1.py
@@ -4,11 +4,10 @@ number=[]
 while(n<int(m)):
     number.append(2**n)
     n=n+1
-<<<<<<< HEAD
+
 print(number)
 
 number2=[2**i for i in range(int(m))]
 print(number2)
-=======
-print(number)
->>>>>>> origin/HomeWork
+
+
